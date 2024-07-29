d1df40c00ac488907420ff3f6f357183c6bb3f87
shajib-khan <shajibkhan946@gmail.com>
2024-07-29 11:48:43 +0600

    start the project

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Livewire\Home\Index;
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
+//custom route
+//Route::get('/',Index::class)->name('home.index');
