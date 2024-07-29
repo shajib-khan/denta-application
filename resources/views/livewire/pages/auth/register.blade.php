93000fc5d41f3696e2aec40b01546ebda9a2a099
shajib-khan <shajibkhan946@gmail.com>
2024-07-29 12:51:43 +0600

    user registration fill done

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -11,6 +11,12 @@ use Livewire\Volt\Component;
 new #[Layout('layouts.guest')] class extends Component
 {
     public string $name = '';
+    public string $phone = '';
+    public string $gender = '';
+    public string $days_select = '';
+    public string $time_select = '';
+    public string $medical_history = '';
+    public string $notes = '';
     public string $email = '';
     public string $password = '';
     public string $password_confirmation = '';
@@ -22,6 +28,12 @@ new #[Layout('layouts.guest')] class extends Component
     {
         $validated = $this->validate([
             'name' => ['required', 'string', 'max:255'],
+            'phone' => ['required'],
+            'gender' => ['required'],
+            'days_select' => ['required'],
+            'time_select' => ['required'],
+            'medical_history' => ['required'],
+            'notes' => ['required'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
         ]);
@@ -39,10 +51,17 @@ new #[Layout('layouts.guest')] class extends Component
 <div>
     <form wire:submit="register">
         <!-- Name -->
-        <div>
+        <div class="mt-4">
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+
+         <!-- Phone -->
+         <div class="mt-4">
+            <x-input-label for="phone" :value="__('Phone')" />
+            <x-text-input wire:model="phone" id="phone" class="block mt-1 w-full" type="number" required autofocus autocomplete="number" />
+            <x-input-error :messages="$errors->get('number')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
@@ -51,6 +70,60 @@ new #[Layout('layouts.guest')] class extends Component
             <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
+
+           <!-- gender Male -->
+           <div class="mt-4">
+            <x-input-label  for="bio" :value="__('Gender')" />
+            <input type="radio" id="male" wire:model="gender" value="1" checked />
+            <label for="male">Male</label>
+          </div>
+
+          <!-- gender Female -->
+          <div>
+            <input type="radio" id="female" wire:model="gender" value="0" />
+            <label for="female">female</label>
+            <x-input-error :messages="$errors->get('gender')" class="mt-2" />
+          </div>
+
+           <!-- Doctor Select -->
+         {{-- <div class="mt-4">
+            <select wire:model="doctor_id">
+                <option value="Select Doctor"</option>
+                    @foreach ($doctors as $doctor)
+                <option value="{{ $doctor->id }}">{{ $docotor->name }}</option>
+                @endforeach
+            </select>
+        </div> --}}
+
+        <!-- Days select -->
+        <div class="mt-4">
+            <x-input-label for="days_select" :value="__('Day Select')" />
+            <x-text-input wire:model="days_select" id="days_select" class="block mt-1 w-full" type="date"/>
+            <x-input-error :messages="$errors->get('days_select')" class="mt-2" />
+        </div>
+
+          <!-- Time select -->
+          <div class="mt-4">
+            <x-input-label for="time_select" :value="__('Time Select')" />
+            <x-text-input wire:model="time_select" id="time_select" class="block mt-1 w-full" type="time"/>
+            <x-input-error :messages="$errors->get('time_select')" class="mt-2" />
+        </div>
+
+         <!-- Medical History -->
+         <div class="mt-4">
+            <x-input-label for="Medical History" :value="__('Medical History')" />
+            <x-text-input wire:model="medical_history" id="medical_history" class="block mt-1 w-full" type="text"/>
+            <x-input-error :messages="$errors->get('medical_history')" class="mt-2" />
+        </div>
+
+         <!-- Notes -->
+         <div class="mt-4">
+            <x-input-label for="Notes" :value="__('Notes')" />
+            <x-text-input wire:model="Notes" id="Notes" class="block mt-1 w-full" type="text"/>
+            <x-input-error :messages="$errors->get('Notes')" class="mt-2" />
+        </div>
+
+
 
         <!-- Password -->
         <div class="mt-4">
